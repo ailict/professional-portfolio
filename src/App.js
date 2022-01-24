efe8486d3a57166bd4e80c53a9c2d75a3c1d7175
ailict <ailichatfieldtaylor@gmail.com>
2022-01-23 21:09:19 -0500

    set up app.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import ReachOut from "./components/ReachOut";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Nav />
+        <main>
+          <div className="container">
+            <Route exact path="/" component={AboutMe} />
+            <Route exact path="/about" component={AboutMe} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/resume" component={Resume} />
+            <Route exact path="/ReachOut" component={ReachOut} />
+          </div>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
