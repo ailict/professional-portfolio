0929f01f297f07d859985ab9e9f7a4c52a7e3d0d
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 18:46:25 -0500

    naming across files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Nav from "./components/Header";
+import Nav from "./components/header";
 import AboutMe from "./components/about";
 import Projects from "./components/projects";
 import Resume from "./components/resume";
