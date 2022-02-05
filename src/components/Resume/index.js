e28c82ab81a0e3644af1b68ee9db3d05249b7f2d
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 18:37:45 -0500

    created resume component from resume html

---
+++ src/components/Resume/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <section id="resume" class="resume">
+    <h2 class="section-title primary-border">Resume</h2>
+    <p>
+      This is resume section placeholder where I'd like to keep my CV.
+    </p>
+  </section>
+  );
+}
+
+export default Resume;
