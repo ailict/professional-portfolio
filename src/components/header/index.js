fbcd424dcff5c78bfb780b19b0dc185b6247a9e3
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 18:42:44 -0500

    header component made from header html

---
+++ src/components/header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Header() {
+    return (
+<header>
+      <h1>
+        <a href="/">
+          Aili Chatfield-Taylor
+        </a>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about-me">About Me</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#resume">Resume</a>
+          </li>
+          <li>
+            <a href="#contact">Reach Out</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+     );
+    }
+    
+    export default Header;
