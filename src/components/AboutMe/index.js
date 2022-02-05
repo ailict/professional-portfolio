6c5b3ee2a5d89eaa6221508a72921b96bec751e1
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 18:30:24 -0500

    updated components structure and created about me component

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <div>
+        <p>
+        This is an about me placeholder. This page is a work in progress.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
