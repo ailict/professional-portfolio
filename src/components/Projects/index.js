3b84989059ab88b97454e18f15d2caac76e6167a
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 18:33:46 -0500

    created project component from project html

---
+++ src/components/Projects/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Projects() {
+    return (
+<section id="projects" class="projects">
+      <h2 class="section-title primary-border">Projects</h2>
+      <div class="flexbox-container">
+
+            <div class="flexbox-item flexbox-item-1">
+                <p> Run Buddy</p>
+                <a href="https://ailict.github.io/run-buddy/">
+                    <img src="public/images/runbuddy.jpg">
+                </a>            
+            </div>
+
+            <div class="flexbox-item flexbox-item-2">
+                <a href="https://github.com/ailict/robot-gladitors">Robot Gladiators link out to Github, not yet a live page</a>
+            </div>
+
+            <div class="flexbox-item flexbox-item-3">
+                <a href="https://github.com/ailict/datenightgenerator">Date Night Generator</a>
+            </div>
+
+            <div class="flexbox-item flexbox-item-4">
+                <a href="https://band-gogh.herokuapp.com/">Band Gogh</a>
+            </div>
+      </div>
+    </section>
+    );
+}
+
+export default Projects;
