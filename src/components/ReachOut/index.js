352595d2af0ddc7ac186fd9fb1396c497d6aca9a
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 18:35:52 -0500

    created reachout component from reachout html

---
+++ src/components/ReachOut/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function ReachOut() {
+  return (
+    <section id="contact" class="contact">
+      <h2 class="section-title secondary-border">Reach Out</h2>
+      <div class="contact-info">
+        <div>
+          <h3>Contact Me</h3>
+          <p>
+            Questions about my projects?
+            <br />
+            Let me know and I'd be happy to talk to you!
+          </p>
+          <address>
+            I should get a PO Box <br />
+            Richmond, VA <br />
+            P: 804.665.3351 <br />
+            E: <a href="mailto:ailichatfieldtaylor@gmail.com">ailichatfieldtaylor@gmail.com</a>
+          </address>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ReachOut;
