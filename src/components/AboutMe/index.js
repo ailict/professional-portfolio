465a7ece8ddb1242851a49103e67eec74eaf19e6
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 18:43:27 -0500

    updated about me component

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 function AboutMe() {
   return (
-    <section>
-      <div>
-        <p>
-        This is an about me placeholder. This page is a work in progress.
-        </p>
-      </div>
+    <section id="about-me" class="intro">
+      <h2 class="section-title primary-border">About Me</h2>
+      <p>
+        This is an about me placeholder. This page is a work in progress, but does contain a photo, linked sections, contact info, and responsive flexboxes that link out.
+      </p>
     </section>
   );
 }
